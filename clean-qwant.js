bc3fafea99e9d500b1d85d245c3cb944966ba7a8
Toorero <toorero6@gmail.com>
2019-06-14 21:03:16 +0200

    Update clean-qwant.js
    - changed background to a more pleasant black in darkmode copied by DarkReader
    - removed extension-install-box on the start page

--- clean-qwant.js
+++ clean-qwant.js
@@ -5,6 +5,7 @@
 // @version  1
 // @run-at document-start
 
+// @namespace https://greasyfork.org/users/213191
 // ==/UserScript==
 
 //create a new style node
@@ -12,7 +13,7 @@ var style = document.createElement('style');
 style.type = 'text/css';
 
 //sets style content
-style.innerHTML = '.verticals__container {display: none !important} .header_content.header_content--classic :not(.header__item--appmenu) {display: none} .header__item--appmenu * { display: block !important} .header__item--appmenu {border-left: none} footer {display: none}';
+style.innerHTML = '.main-background-color, .wrapper, footer {background-color: #181a1b} .home__snippet__extension {display: none} .verticals__container {display: none !important} .header_content.header_content--classic :not(.header__item--appmenu) {display: none} .header__item--appmenu * { display: block !important} .header__item--appmenu {border-left: none} footer {display: none}';
 
 //append style to document
 document.getElementsByTagName('head')[0].appendChild(style);
