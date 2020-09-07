a1d335618fba68494d08b9f9520b7361d07df90a
Toorero <toorero6@gmail.com>
2020-09-07 02:26:23 +0200

    reverted back to old method of including the changed style

--- clean-qwant.js
+++ clean-qwant.js
@@ -2,18 +2,18 @@
 // @name     Clean Qwant
 // @description This scipt will result in a more minimalistic version of the Qwant website.
 // @include http*://*.qwant.*
-// @version  2.0
+// @version  1.1
 // @run-at document-start
 
 // @namespace https://greasyfork.org/users/213191
 // ==/UserScript==
 
 //create a new style node
-var style = document.createElement('link');
-style.rel = 'stylesheet';
+var style = document.createElement('style');
+style.type = 'text/css';
 
-//sets style content
-style.href = 'https://raw.githubusercontent.com/Toorero/clean-qwant/master/style_change.css';
+//sets style content (see style_change.css)
+style.innerHTML = '.wrapper{background-color:#181a1b!important},.all_content,.header_content.header_content--classic :not(.header__item--appmenu),.home__snippet__extension,.sidebar__item.sidebar__item--maps,.sidebar__item.sidebar__item--music,.sidebar__item.sidebar__item--shopping,.verticals__container,footer{display:none!important}.header__item--appmenu *{display:block!important}.header__item--appmenu{border-left:none}';
 
 //append style to document
 document.getElementsByTagName('head')[0].appendChild(style);
