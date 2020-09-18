08642ef5d3f4848b93f5d02d5babfb094e95b46b
Toorero <toorero6@gmail.com>
2020-09-18 19:02:56 +0200

    Proper disable of news feed

--- clean-qwant.js
+++ clean-qwant.js
@@ -13,7 +13,7 @@ var style = document.createElement('style');
 style.type = 'text/css';
 
 //sets style content (see style_change.css)
-style.innerHTML = '.wrapper{background-color:#181a1b!important}.all_content,.header_content.header_content--classic :not(.header__item--appmenu),.home__snippet__extension,.sidebar__item.sidebar__item--maps,.sidebar__item.sidebar__item--music,.sidebar__item.sidebar__item--shopping,.verticals__container,footer{display:none!important}.header__item--appmenu *{display:block!important}.header__item--appmenu{border-left:none}';
+style.innerHTML = '.wrapper{background-color:#181a1b!important}.all_content,.header_content.header_content--classic :not(.header__item--appmenu),.home__snippet__extension,.sidebar__item.sidebar__item--maps,.sidebar__item.sidebar__item--music,.sidebar__item.sidebar__item--shopping,.verticals__container,footer,.result-smart__news{display:none!important}.header__item--appmenu *{display:block!important}.header__item--appmenu{border-left:none}';
 
 //append style to document
 document.getElementsByTagName('head')[0].appendChild(style);
